755cc9945bd6b919118e8c513c8e907299b321f9
Wacek <wacek@cdw2.(none)>
2012-10-07 23:00:29 +0200

    Initial commit

---
+++ wincomport/Uart.h
@@ -0,0 +1,9 @@
+
+#include <windows.h>
+#include <commctrl.h>
+
+int SetupUart(int ComPortNr=1,int baud=9600,int Bitsize=8,int StopBits=1,int Parity=NOPARITY);
+int WriteUart(unsigned char *buf, int len);
+int ReadUart(unsigned char *buf, int len);
+int CloseUart();
+
